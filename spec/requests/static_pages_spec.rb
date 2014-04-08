c049faf5d855b9baadf64123976d98b0ba93e9a0
William Tandiawan <william.tandiawan@gmail.com>
2014-04-08 16:54:27 -0500

    Home page not yet routed - placeholder

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Storyboard'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Storyboard')
+    end
+  end
+end
