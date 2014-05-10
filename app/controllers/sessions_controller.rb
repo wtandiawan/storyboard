cd017da577492cb096a0eb3b1280bf416673d91b
William Tandiawan <william.tandiawan@gmail.com>
2014-05-10 14:03:14 -0500

    Signin

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+    render 'new'
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = "Invalid email/password combination"
+      # Create an error message and re-render the signin form.
+    end
+  end
+
+  def destroy
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
@@ -55,11 +52,8 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    sign_out
+    redirect_to root_url
   end
 
   private
