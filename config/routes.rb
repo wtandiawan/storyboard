e634e1f29e0333ec61c970540f379deb9e133455
William Tandiawan <william.tandiawan@gmail.com>
2014-05-10 13:38:54 -0500

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Storyboard::Application.routes.draw do
-  resources :stories
-
   resources :users
-
   root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
